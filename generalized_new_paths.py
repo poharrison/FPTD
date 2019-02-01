2040bbaa580decf308215664b39f596167d18e72
Page Harrison <page.harrison.o@gmail.com>
2019-02-01 15:55:49 -0500

    Add exception handling for user input of number of bins.

--- generalized_new_paths.py
+++ generalized_new_paths.py
@@ -1,13 +1,30 @@
 def get_bin_lists(bins_list_name):
     while True:
         try:
-            bins_list = list(int(x) for x in input("Please list the " + bins_list_name + " (Ex. 1, 2, 3, 4): ").split(","))
+            bins_list = list(int(x) for x in raw_input("Please list the %s (Ex. 1, 2, 3, 4): " % bins_list_name).split(","))
             return bins_list
         except ValueError:
             print("Please check the formatting of your input. Input should be a list of of numbers separated by commas.")
 
 
+def get_nbins():
+    while True:
+        try:
+            NBINS = int(input("Total number of bins: "))
+            return NBINS
+        except (NameError, TypeError):
+            print("Please input a single number.")
+
+
+NBINS = get_nbins()
+INIT_BINS = get_bin_lists("initial bins")
+if INIT_BINS.length > NBINS:
+    print("Cannot have more initial bins than bins total.")
+    INIT_BINS = get_bin_lists("initial bins")
 TARGET_BINS = get_bin_lists("target bins")
+if TARGET_BINS.length > NBINS:
+    print("Cannot have more target bins than bins total.")
+    INIT_BINS = get_bin_lists("target bins")
 
 for x in TARGET_BINS:
     print(x)
